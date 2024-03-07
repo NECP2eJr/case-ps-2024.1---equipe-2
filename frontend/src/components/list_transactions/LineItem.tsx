5da1703d5971c0e3ed4c0dfd424ecda55cb8bbe3
Vinícius Santos <viniciusssantos.pa@gmail.com>
2024-03-06 22:48:43 -0300

    commit lineitems and transactions

--- frontend/src/components/list_transactions/LineItem.tsx
+++ frontend/src/components/list_transactions/LineItem.tsx
@@ -1,10 +1,10 @@
 export default function LineItems({ name, price, type, date }) {
     return (
         <tr>
-            <td>Desenvolvimento de site</td>
-            <td>R$ 12.000,00</td>
-            <td>Venda</td>
-            <td>10/12/2024</td>
+            <td>{name}</td>
+            <td>{price}</td>
+            <td>{type}</td>
+            <td>{date}</td>
         </tr>
     )
 }

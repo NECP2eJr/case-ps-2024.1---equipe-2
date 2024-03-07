873dffa3bf4646f1a5d5a7c34e97773875c0ca4f
Gabriel Parreiras <gparreiras46@gmail.com>
2024-03-06 22:04:57 -0300

    header

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <header>
+        <div className="containerLogo">
+          <div>
+              LOGO
+          </div>
+          <div>
+            CPMoney
+          </div>
+        </div>
+
+        <button>
+            Cadastrar nova transação
+        </button>
+      </header>
+  )
+}
+
+export default Header

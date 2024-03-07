0d60bf844098481761945d5e8d080441cd83a39c
Gabriel Parreiras <gparreiras46@gmail.com>
2024-03-06 22:20:48 -0300

    add transactions container

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import Transactions from "./components/transactions/Transactions";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Transactions/>
     </div>
   )
 }
---
+++ frontend/src/components/transactions/Transactions.tsx
@@ -0,0 +1,27 @@
+const Transactions = () => {
+
+    let entradas = 12.000
+    let saidas = 3.450
+
+
+  return (
+    <div className="containerTransactions">
+
+      <div className="containerEntradas">
+        <p>
+            R$ {entradas},00
+        </p>
+      </div>
+
+      <div className="containerSaidas">
+            R$ {saidas},00
+      </div>
+
+      <div className="containerTotal">
+            R$ {entradas - saidas},00
+      </div>
+    </div>
+  );
+};
+
+export default Transactions;
